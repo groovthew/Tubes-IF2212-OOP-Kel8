21a585bbc3e97529c8dfad7c5280e48955fe16ff
groovthew <140098390+groovthew@users.noreply.github.com>
2024-05-01 22:36:11 +0700

    Sunflower

--- src/Tanaman/Sunflower.java
+++ src/Tanaman/Sunflower.java
@@ -1,26 +1,14 @@
 package Tanaman;
 
 public class Sunflower extends Plant {
-    private int sun = 25;
-    private int interval = 10;
+    private int sun;
 
     public Sunflower(int sun, int interval) {
         super("Sunflower", 50, 100, 0, 0, 0, 10);
-        this.sun = sun;
-        this.interval = interval;
+        this.sun = 25;
     }
 
     public int getSun() {
         return sun;
-    }
-
-    public int getInterval() {
-        return interval;
-    }
-
-    public void setInterval(int sun) {
-        for(int i = 0; i <= interval; i++) {
-            this.sun = sun;
-        }
     }
 }
