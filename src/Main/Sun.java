63dfbf2bdb82acd2f1931b88e4b4a647535a5de8
groovthew <140098390+groovthew@users.noreply.github.com>
2024-05-01 22:42:45 +0700

    Sun

--- src/Main/Sun.java
+++ src/Main/Sun.java
@@ -1,5 +1,18 @@
 package Main;
 
 public class Sun {
+    private int totalSun;
     
+    public Sun(int totalSun) {
+        this.totalSun = 25;
+    }
+
+    public int getTotalSun() {
+        return totalSun;
+    }
+
+    public void addTotalSun() {
+        totalSun += totalSun;
+    }
 }
+
