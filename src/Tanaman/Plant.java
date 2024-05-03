ec9b5330ba534828724947c101fa86a88066b497
Kevinzaa <fkevinza@gmail.com>
2024-05-03 17:09:32 +0700

    Update Plant.java

--- src/Tanaman/Plant.java
+++ src/Tanaman/Plant.java
@@ -1,46 +1,24 @@
 package Tanaman;
 
-public class Plant {
+import Main.Character;
+
+public class Plant extends Character{
 
     // Mendeklarasikan atribut
-    private String name;
     private int cost;
-    private int health;
-    private int attack_damage;
-    private int attack_speed;
     private int range;
-    private int cool_down;
+    private int cooldown;
 
     // Method
-    public Plant(String name, int cost, int health, int attack_damage, int attack_speed, int range, int cool_down) {
-
+    public Plant(String name, int health, int attack_damage, int attack_speed, int cost, int range, int cooldown) {
+        super(name, health, attack_damage, attack_speed);
+        this.cost = cost;
+        this.range = range;
+        this.cooldown = cooldown;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getCost() {
-        return cost;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public int getAttackDamage() {
-        return attack_damage;
-    }
-
-    public int getAttackSpeed() {
-        return attack_speed;
-    }
-
-    public int getRange() {
-        return range;
-    }
-
-    public int getCoolDown() {
-        return cool_down;
-    }
+    //getter
+    public int getCost(){ return cost;}
+    public int getRange(){ return range;}
+    public int getCooldown(){ return cooldown;}
 }
