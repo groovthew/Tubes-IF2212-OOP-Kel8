d7fe3071c13b89ca6f9f3d36909d4a9664bb2f28
nicolaasheru <118559021+nicolaasheru@users.noreply.github.com>
2024-05-08 10:13:38 +0700

    Update SnowPea.java
    gambaran

--- src/Tanaman/SnowPea.java
+++ src/Tanaman/SnowPea.java
@@ -1,6 +1,7 @@
 package Tanaman;
 
 import Grid.Tile;
+import Zombie.Zombie;
 
 public class SnowPea extends Plant {
     public SnowPea(String name, int health, int attack_damage, int attack_speed, int cost, int range, int cooldown) {
@@ -9,7 +10,12 @@ public class SnowPea extends Plant {
 
     public void shootSnowPea(Tile tile) {
         if (tile.hasZombie()) {
-            
+            Zombie zombie = tile.getZombie();
+            System.out.println("Snow Pea shoots a frozen pea at the zombie!");
+            zombie.takeDamage(getAttackDamage()); // gambaran
+            zombie.freeze(); // ini harus ada method ini di zombie(?)
+        } else {
+            System.out.println("No zombie on this tile to shoot at.");
         }
     }
 }
