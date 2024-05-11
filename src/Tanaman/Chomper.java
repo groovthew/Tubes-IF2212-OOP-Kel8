e2f029cddb859d159993d82d93b1dcae254e42c8
nicolaasheru <118559021+nicolaasheru@users.noreply.github.com>
2024-05-11 19:33:02 +0700

    Update Chomper.java

--- src/Tanaman/Chomper.java
+++ src/Tanaman/Chomper.java
@@ -1,11 +1,21 @@
 package Tanaman;
 
 public class Chomper extends Plant {
+    private boolean isEating; // Untuk menandai apakah Chomper sedang makan atau tidak
+    private int eatingCooldown; // Waktu cooldown setelah Chomper makan
+
     public Chomper(String name, int health, int attack_damage, int attack_speed, int cost, int range, int cooldown) {
         super("Chomper", 1000, 1000, 0, 150, 0, 10);
+        this.isEating = false;
+        this.eatingCooldown = 0;
     }
 
     public void eatingCoolDown() {
-        
+        if (isEating) {
+            eatingCooldown--;
+            if (eatingCooldown <= 0) {
+                isEating = false;
+            }
+        }
     }
 }
