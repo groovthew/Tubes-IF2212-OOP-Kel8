36033c20f89a23d4177683abede2f76b96b1f6b2
nicolaasheru <118559021+nicolaasheru@users.noreply.github.com>
2024-05-18 13:22:54 +0700

    Update DolphinRiderZombie.java

--- src/Zombie/DolphinRiderZombie.java
+++ src/Zombie/DolphinRiderZombie.java
@@ -1,7 +1,31 @@
 package Zombie;
 
 public class DolphinRiderZombie extends Zombie {
+    private boolean isJumped;
+    
     public DolphinRiderZombie(){
-        super("Dolphin Rider Zombie", 175, 100, 1, true);    
+        super("Dolphin Rider Zombie", 175, 100, 1, true);  
+        this.isJumped = false;  
+    }
+    
+    public boolean isJumped() {
+        return isJumped;
+    }
+
+    public void setJumped(boolean isJumped) {
+        this.isJumped = isJumped;
+    }
+
+    public void jump() {
+        if (!isJumped) {
+            System.out.println("Dolphin Rider Zombie jumps over the first plant!");
+            this.setJumped(true);
+            // this.setSpeed(this.getSpeed() / 2); // ini hrsnya thread jg
+        }
+    }
+
+    public static void main(String[] args) {
+        DolphinRiderZombie drz = new DolphinRiderZombie();
+        drz.jump();
     }
 }
