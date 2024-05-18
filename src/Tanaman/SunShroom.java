fe90890430644960bfb56bdf89baa179c48c0a08
Kevinzaa <fkevinza@gmail.com>
2024-05-18 22:04:02 +0700

    SunShroom udah bisa ngehasilin sun tapi belum diatur hanya kalau malem

--- src/Tanaman/SunShroom.java
+++ src/Tanaman/SunShroom.java
@@ -1,22 +1,58 @@
 package Tanaman;
 
+import java.util.Random;
+
+import Sun.ProduceSun;
+import Sun.SunListener;
+
 // import Sun.ProduceSun;
 
-public class SunShroom extends Plant {
-    // private int amount;
+public class SunShroom extends Plant implements ProduceSun{
+    private int sunTotal;
+    private boolean makingSun;
+    private SunListener listener;
 
     public SunShroom(String name, int health, int attack_damage, int attack_speed, int cost, int range, int cooldown) {
         super("Sun-Shroom", 100, 0, 0, 15, 0, 5, false);
     }
 
-    // @Override
-    // public void increaseSun() {
-    //     // TODO Auto-generated method stub
-    //     throw new UnsupportedOperationException("Unimplemented method 'increaseSun'");
-    // }
+    public void setSunListener(SunListener listener) {
+        this.listener = listener;
+    }
 
-    // @Override
-    // public int getAmount() {
-    //    return amount;
-    // }
+    public void increaseSun(){
+        sunTotal += 25;
+        makingSun = true;
+        if (listener != null) {
+            listener.onSunProduced();
+        }
+    }
+
+    @Override
+    public int getAmount(){
+        return sunTotal;
+    }
+
+    @Override
+    public void startProducingSun(){
+        Thread thread = new Thread(() -> {
+            Random random = new Random();
+            while (true) {
+                try {
+                    Thread.sleep((random.nextInt(6) + 5) * 1000);
+                    synchronized (this) {
+                        increaseSun();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+
+    @Override
+    public void stopProducingSun(){
+        makingSun = false;
+    }
 }
