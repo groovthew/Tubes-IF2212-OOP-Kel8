d2a19faee2394440fc88d271237f0921d4010ae1
Kevinzaa <fkevinza@gmail.com>
2024-05-23 19:30:12 +0700

    polevaulting valid

--- src/Zombie/PoleVaultingZombie.java
+++ src/Zombie/PoleVaultingZombie.java
@@ -1,6 +1,7 @@
 package Zombie;
 
 import Map.Tile;
+import Tanaman.Plant;
 
 public class PoleVaultingZombie extends Zombie {
     private boolean hasJumped = false;
@@ -15,20 +16,18 @@ public class PoleVaultingZombie extends Zombie {
 
     // Method untuk melakukan lompatan
     public void jumpTile(Tile[][] tiles, int i, int j) {
-        // Jika sudah melompat, kembalikan method
         if (hasJumped) {
             return;
         }
         // Pastikan bahwa tile berikutnya berada dalam batas dan terdapat tanaman
-        if (j > 0 && !tiles[i][j - 1].getPlants().isEmpty()) {
-            // Pastikan bahwa tile dua langkah ke kiri berada dalam batas
-            if (j - 2 >= 0) {
-                // Pindahkan zombie ke tile dua langkah ke kiri
+        if (j >= 2 && !tiles[i][j - 1].getPlants().isEmpty()) {
+                Plant plant = tiles[i][j - 1].getPlants().get(0);
+                tiles[i][j - 1].getPlants().remove(plant);
                 tiles[i][j].getZombies().remove(this);
                 tiles[i][j - 2].getZombies().add(this);
                 hasJumped = true;
                 System.out.println(getName() + " jumped from tile [" + i + "][" + j + "] to tile [" + i + "][" + (j - 2) + "]");
-            } 
-        }
+        } 
     }
+    
 }
